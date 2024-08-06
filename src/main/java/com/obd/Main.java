8fd3a3b9ab5f446e6236038b793c688856c43366
Alex <systemsagnx@gmail.com>
2024-08-06 10:08:46 -0600

    To test with comm obd2 cable

--- src/main/java/com/obd/Main.java
+++ src/main/java/com/obd/Main.java
@@ -1,6 +1,5 @@
 package com.obd;
 
-
 import com.fazecast.jSerialComm.SerialPort;
 import com.obd.pires.commands.control.VinCommand;
 import com.obd.pires.commands.protocol.EchoOffCommand;
@@ -19,7 +18,10 @@ public class Main {
         Arrays.stream(commPorts).forEach(System.out::println);
         SerialPort comPort = commPorts[2];
         System.out.println("Connected to: " + comPort.getSystemPortName());
-        comPort.setBaudRate(9600);
+        comPort.setBaudRate(38400);
+        comPort.setNumDataBits(8);
+        comPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
+        comPort.setParity(SerialPort.NO_PARITY);
 
         if (comPort.openPort()) {
             System.out.println("Port opened successfully.");
@@ -32,16 +34,24 @@ public class Main {
              OutputStream outStream = comPort.getOutputStream()) {
 
             // Execute commands
+            System.out.println("Executing EchoOffCommand");
             new EchoOffCommand().run(inStream, outStream);
+            System.out.println("Executing LineFeedOffCommand");
             new LineFeedOffCommand().run(inStream, outStream);
+            System.out.println("Executing TimeoutCommand");
             new TimeoutCommand(2500).run(inStream, outStream);
+            System.out.println("Executing SelectProtocolCommand");
             new SelectProtocolCommand(ObdProtocols.ISO_9141_2).run(inStream, outStream);
             System.out.println("Getting Vin");
             new VinCommand().run(inStream, outStream);
             System.out.println("Getting Ambient Temperature");
             new AmbientAirTemperatureCommand().run(inStream, outStream);
 
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
+            System.err.println("IOException occurred: " + e.getMessage());
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            System.err.println("InterruptedException occurred: " + e.getMessage());
             e.printStackTrace();
         } finally {
             comPort.closePort();
