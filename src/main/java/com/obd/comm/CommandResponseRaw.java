4f23c537292fcdef2e469c9f78abb76de5a969ce
AlexDrco <systemsagnx@gmail.com>
2024-08-25 13:12:57 -0600

    Removed simulated boolean

--- src/main/java/com/obd/comm/CommandResponseRaw.java
+++ src/main/java/com/obd/comm/CommandResponseRaw.java
@@ -4,19 +4,11 @@ public class CommandResponseRaw {
     private final String command;
     private final String response;
     private final String raw;
-    private boolean simulated = false;
 
     public CommandResponseRaw(String command, String response, String raw) {
         this.command = command;
         this.response = response;
         this.raw = raw;
-    }
-
-    public CommandResponseRaw(String command, String response, String raw, boolean simulated) {
-        this.command = command;
-        this.response = response;
-        this.raw = raw;
-        this.simulated = simulated;
     }
 
     public CommandResponseRaw(String response){
@@ -35,9 +27,5 @@ public class CommandResponseRaw {
 
     public String getRaw() {
         return raw;
-    }
-
-    public boolean isSimulated() {
-        return simulated;
     }
 }
